b24d16e36e5577866701cd3676b07eeba92674a6
Rostyslav Tyshko <tyshko1@gmail.com>
2023-06-16 13:37:47 +0000

    fix tests

--- staking_contract/staking_contract/src/staking_contract.rs
+++ staking_contract/staking_contract/src/staking_contract.rs
@@ -43,8 +43,8 @@ pub trait CEP20STK<Storage: ContractStorage>: ContractContext<Storage> {
         if staking_starts < u64::from(runtime::get_blocktime()) {
             return Err(Error::StakingStartsNow);
         }
-        if withdraw_ends != withdraw_starts {
-            return Err(Error::StakingStartsNow);
+        if staking_ends != withdraw_starts {
+            return Err(Error::GapBetweenStakingEndsWithdrawStarts);
         }
         data::set_name(name);
         data::set_address(address);
